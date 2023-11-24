fed50c4be0107bd82dd3740a21eeddad243fbad4
elguesabal <joseelguesabal@gmail.com>
2023-11-23 22:55:18 -0300

    retirando o anuncio de venha ser patrocinador e adicionado paginas de outras rodadass

--- js/ad_index.js
+++ js/ad_index.js
@@ -3,8 +3,10 @@ import badbons from "./badbons.js";
 import new_ad from "./new_ad.js";
 
 const space_ad = document.getElementById("ad");
-const ad = [boutique(), badbons(), new_ad()];
-const n = [0, 1, 2];
+// const ad = [boutique(), badbons(), new_ad()];
+// const n = [0, 1, 2];
+const ad = [boutique(), badbons()];
+const n = [0, 1];
 
 for (let i = n.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
