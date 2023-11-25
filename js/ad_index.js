e1220d065634a4c93cc0d49c9cca5f90bda41c07
elguesabal <joseelguesabal@gmail.com>
2023-11-24 23:14:21 -0300

    atualizando

--- js/ad_index.js
+++ js/ad_index.js
@@ -1,6 +1,6 @@
 import boutique from "./boutique.js";
 import badbons from "./badbons.js";
-import new_ad from "./new_ad.js";
+// import new_ad from "./new_ad.js";
 
 const space_ad = document.getElementById("ad");
 // const ad = [boutique(), badbons(), new_ad()];
