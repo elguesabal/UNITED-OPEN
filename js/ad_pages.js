a3ee343aec8f9fd3579a462e36877bee3806a561
elguesabal <joseelguesabal@gmail.com>
2023-11-25 23:04:11 -0300

    atualizando

--- js/ad_pages.js
+++ js/ad_pages.js
@@ -1,8 +1,10 @@
 import boutique from "./boutique.js";
 import badbons from "./badbons.js";
-import new_ad from "./new_ad.js";
+//import new_ad from "./new_ad.js";
 
-const ad = [boutique(), badbons(), new_ad()];
-const ad_random = Math.floor(Math.random() * 3);
+// const ad = [boutique(), badbons(), new_ad()];
+// const ad_random = Math.floor(Math.random() * 3);
+const ad = [boutique(), badbons()];
+const ad_random = Math.floor(Math.random() * 2);
 
 document.getElementById("ad").innerHTML = ad[ad_random];
