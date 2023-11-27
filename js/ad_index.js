bca3d8a205015d4d1659efa098dac68d173f4dad
elguesabal <joseelguesabal@gmail.com>
2023-11-26 23:25:09 -0300

    retirando anuncio da badbons

--- js/ad_index.js
+++ js/ad_index.js
@@ -1,19 +1,21 @@
 import boutique from "./boutique.js";
-import badbons from "./badbons.js";
+// import badbons from "./badbons.js";
 // import new_ad from "./new_ad.js";
 
-const space_ad = document.getElementById("ad");
+// const space_ad = document.getElementById("ad");
 // const ad = [boutique(), badbons(), new_ad()];
 // const n = [0, 1, 2];
-const ad = [boutique(), badbons()];
-const n = [0, 1];
 
-for (let i = n.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    let temp = n[i];
-    n[i] = n[j];
-    n[j] = temp;
-}
-for (let i = 0; i < ad.length; i++) {
-    space_ad.innerHTML += ad[n[i]];
-}
+// const ad = [boutique(), badbons()];
+// const n = [0, 1];
+// for (let i = n.length - 1; i > 0; i--) {
+//     let j = Math.floor(Math.random() * (i + 1));
+//     let temp = n[i];
+//     n[i] = n[j];
+//     n[j] = temp;
+// }
+// for (let i = 0; i < ad.length; i++) {
+//     space_ad.innerHTML += ad[n[i]];
+// }
+
+document.getElementById("ad").innerHTML = boutique();
--- js/ad_pages.js
+++ js/ad_pages.js
@@ -1,10 +1,13 @@
 import boutique from "./boutique.js";
-import badbons from "./badbons.js";
-//import new_ad from "./new_ad.js";
+// import badbons from "./badbons.js";
+// import new_ad from "./new_ad.js";
 
 // const ad = [boutique(), badbons(), new_ad()];
 // const ad_random = Math.floor(Math.random() * 3);
-const ad = [boutique(), badbons()];
-const ad_random = Math.floor(Math.random() * 2);
+// document.getElementById("ad").innerHTML = ad[ad_random];
 
-document.getElementById("ad").innerHTML = ad[ad_random];
+// const ad = [boutique(), badbons()];
+// const ad_random = Math.floor(Math.random() * 2);
+// document.getElementById("ad").innerHTML = ad[ad_random];
+
+document.getElementById("ad").innerHTML = boutique();
